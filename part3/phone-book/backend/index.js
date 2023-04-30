4c21f0dec15b44870889294dccf793b8822ffa47
beachf1res <lisaxbarkar@gmail.com>
2023-04-30 20:34:45 +0300

    3.7-3.8

--- part3/phone-book/backend/index.js
+++ part3/phone-book/backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 let persons = [
   {
@@ -30,7 +31,12 @@ function generateId() {
 }
 
 const app = express();
+morgan.token('body', (req) => JSON.stringify(req.body));
+
 app.use(express.json());
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
