ef78af93c745dd9099477a7829a9ec7efb2f6484
beachf1res <lisaxbarkar@gmail.com>
2023-04-30 12:19:53 +0300

    2.11

--- part2/phone-book/src/App.js
+++ part2/phone-book/src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Filter, NewPersonForm, Persons } from './components';
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
   const [filter, setFilter] = useState('');
   const [shownPersons, setShownPersons] = useState(persons);
+
+  useEffect(() => {
+    const fetcher = async () => {
+      const response = await axios.get('http://localhost:3001/persons');
+      setPersons(response.data);
+    };
+
+    fetcher().catch(console.error);
+  }, []);
 
   useEffect(() => {
     setShownPersons(persons);
