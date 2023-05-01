932efebb26c50d4b96098735a03ac013530a6898
beachf1res <lisaxbarkar@gmail.com>
2023-05-01 14:05:02 +0300

    add removal

--- part3/phone-book/backend/index.js
+++ part3/phone-book/backend/index.js
@@ -35,14 +35,16 @@ function generateId() {
 }
 
 const app = express();
-morgan.token('body', (req) => JSON.stringify(req.body));
 
-app.use(cors());
-app.use(express.json());
-app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+morgan.token('body', (req) => JSON.stringify(req.body));
+const requestLogger = morgan(
+  ':method :url :status :res[content-length] - :response-time ms :body'
 );
+
 app.use(express.static('build'));
+app.use(express.json());
+app.use(cors());
+app.use(requestLogger);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>');
@@ -62,7 +64,7 @@ app.get('/api/persons', (req, res) => {
   });
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
   const { id } = req.params;
   Person.findById(id)
     .then((person) => {
@@ -72,17 +74,14 @@ app.get('/api/persons/:id', (req, res) => {
         res.status(404).end();
       }
     })
-    .catch((e) => {
-      console.error(e);
-      res.status(400).send({ error: 'Malformed id' });
-    });
+    .catch((e) => next(e));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   const { id } = req.params;
-  persons = persons.filter((p) => p.id !== Number(id));
-
-  res.status(204).end();
+  Person.findByIdAndRemove(id)
+    .then(() => res.status(204).end())
+    .catch((e) => next(e));
 });
 
 app.post('/api/persons', (req, res) => {
@@ -101,6 +100,22 @@ app.post('/api/persons', (req, res) => {
 
   person.save().then((savedPerson) => res.json(savedPerson));
 });
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'Uknown Endpoint' });
+};
+app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
